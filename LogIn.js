5d7ab949217ef1364fae09fb2fc0d2c4be471628
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-06-30 19:48:33 +0530

    final Update

--- LogIn.js
+++ LogIn.js
@@ -15,15 +15,22 @@ let btn_signup = document.querySelector(".btn_signup");
 let username = document.querySelector(".username");
 let loginpass = document.querySelector(".password");
 let btn_login = document.querySelector(".btn_login");
-
+let p = document.createElement("p");
 //link click facility...
 signup_btnlnk.addEventListener("click", function () {
   loginForm.classList.add("hidden");
   signupForm.classList.remove("hidden");
+  username.value = "";
+  loginpass.value = "";
 });
 login_btnlnk.addEventListener("click", function () {
   loginForm.classList.remove("hidden");
   signupForm.classList.add("hidden");
+  fname.value = "";
+  email.value = "";
+  pass.value = "";
+  phone.value = "";
+  p.textContent = "";
 });
 
 let arr = JSON.parse(localStorage.getItem("users"));
@@ -31,51 +38,68 @@ if (arr == null) {
   arr = [];
 }
 btn_signup.addEventListener("click", function () {
-  const details = {
-    fstname: fname.value,
-    email: email.value,
-    paswrd: pass.value,
-    phone: phone.value,
-  };
+  if (
+    fname.value !== "" &&
+    email.value !== "" &&
+    pass.value !== "" &&
+    phone.value !== ""
+  ) {
+    var details = {
+      fstname: fname.value,
+      email: email.value,
+      paswrd: pass.value,
+      phone: phone.value,
+    };
+    arr.push(details);
+  }
+
   //console.log(details);
-  arr.push(details);
+
   localStorage.setItem("users", JSON.stringify(arr));
 
-  let p = document.createElement("p");
-  if (details.fstname !== "") {
-    p.textContent = "✅Account created successfull !";
+  // let p = document.createElement("p");
+  if (
+    fname.value !== "" &&
+    email.value !== "" &&
+    pass.value !== "" &&
+    phone.value !== ""
+  ) {
+    p.textContent = "✅Account creation successfull !";
     signupForm.append(p);
-    console.log(this.fstname);
-    // setTimeout(()=>{
-    //   window.location.assign("index.html");
-    // },3000);
+  } else {
+    alert("Please fill out all the fields!");
   }
 });
 
-let p = document.createElement("p");
+// let p = document.createElement("p");
 const displayMessage = function (message) {
   p.textContent = message;
   loginForm.append(p);
 };
 let isvalid = false;
 
-let check=JSON.parse(localStorage.getItem("loginStatus"));
-if(check==null){
-  check=[];
+let check = JSON.parse(localStorage.getItem("loginStatus"));
+if (check == null) {
+  check = [];
 }
 let status;
 
 btn_login.addEventListener("click", function () {
+  if (arr.length === 0 || (username.value === "" && loginpass.value === "")) {
+    alert(`Username & password field can't be empty!
+    `);
+    btn_login.removeEventListener();
+  }
   for (let i = 0; i < arr.length; i++) {
     if (
       (username.value === arr[i].email || username.value === arr[i].phone) &&
       loginpass.value === arr[i].paswrd
     ) {
       isvalid = true;
-      check=[];
-      status='success';
-      check.push(status,true,arr[i].fstname);
-      localStorage.setItem('loginStatus', JSON.stringify(check));
+      check = [];
+      status = "success";
+      check.push(status, true, arr[i].fstname);
+      localStorage.setItem("loginStatus", JSON.stringify(check));
       displayMessage(`Welcome back, ${arr[i].fstname}!`);
       setTimeout(() => {
         window.location.assign("index.html");
@@ -84,11 +108,10 @@ btn_login.addEventListener("click", function () {
   }
   if (!isvalid) {
     displayMessage("❌ Invalid username or password");
-      check=[];
-      status='unsuccess';
-      localStorage.removeItem('loginStatus', JSON.stringify(check));
-      check.push(status,false);
-      localStorage.setItem('loginStatus', JSON.stringify(check));
+    check = [];
+    status = "unsuccess";
+    localStorage.removeItem("loginStatus", JSON.stringify(check));
+    check.push(status, false);
+    localStorage.setItem("loginStatus", JSON.stringify(check));
   }
 });
-
